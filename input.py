4932410fd1dc13fc3b4423d68b0259f7e6a067e2
23BAI70229 <asmitsachan28@gmail.com>
2026-09-16 02:48:24 +0530

    Create input.py

---
+++ input.py
@@ -0,0 +1,16 @@
+                              # learn how to take input from user And (use of comments)
+
+
+name = input("Enter your name: ")
+rollno = int(input("Enter your roll number: "))
+print(name, rollno)
+
+
+                         #unit one exercise 
+#add a person with first name as Tony and last name as Stark
+#Tony's age is 53
+#Tony's height is 1.85m
+#Tony is secretly a superhero. Take his superhero name as input & print it
+
+name = input("Enter your last name: ")
+print("tony"+name)
