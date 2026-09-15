cb20d3e2bbc6797c0b9c78dc6f36d612621b6b6d
23BAI70229 <asmitsachan28@gmail.com>
2026-09-16 02:48:38 +0530

    Create variable.py

---
+++ variable.py
@@ -0,0 +1,13 @@
+name="asmit"
+rollno=229
+
+name="somil"
+rollno=2
+cgpa=9.5
+is_student=True
+
+print(name,rollno,cgpa)
+print(type(name))
+print(type(rollno))
+print(type(cgpa))
+print(type(is_student))
